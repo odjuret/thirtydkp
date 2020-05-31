42cb439b1762765738a287f35e58e71e674756ce
Jimmie <sundberg.jimmie@gmail.com>
2020-05-31 21:13:15 +0200

    extracted out data into DAL layer

---
+++ DAL.lua
@@ -0,0 +1,22 @@
+local addonName, addonNamespace = ... 
+
+-- Initializing the core global variables
+addonNamespace.DAL = {}  
+
+local DAL = addonNamespace.DAL
+
+function DAL:Initialize()
+    ------------------------------------
+    --	Import SavedVariables
+    ------------------------------------
+    -- saved variables starts as nil. we want a empty table
+    if not ThirtyDKP_Database_DKPTable then ThirtyDKP_Database_DKPTable = {} end;
+    DAL.DKPTableCopy 		= ThirtyDKP_Database_DKPTable;	
+    DAL.DKPTableNumRows     = 0;
+    if #ThirtyDKP_Database_DKPTable > 0 then DAL.DKPTableNumRows = #ThirtyDKP_Database_DKPTable end;
+
+    table.sort(ThirtyDKP_Database_DKPTable, function(a, b)
+        return a["player"] < b["player"]
+    end)
+    
+end

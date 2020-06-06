7760591831ae6a515ea6c757d111147f05a1981a
unknown <antonroennbaeck@gmail.com>
2020-06-06 20:23:42 +0200

    Added table_search

--- Core/ManageDKPTableEntries.lua
+++ Core/ManageDKPTableEntries.lua
@@ -42,8 +42,12 @@ function Core:AddGuildToDKPTable()
         nameFromGuild, rank, rankIndex, _, tempClass = GetGuildRosterInfo(i)
         nameFromGuild = strsub(nameFromGuild, 1, string.find(nameFromGuild, "-")-1) -- required to remove server name from player (can remove in classic if this is not an issue)
         
-        if DAL:AddToDKPTable(nameFromGuild, tempClass) then
-            print("ThirtyDKP: added "..nameFromGuild.." successfully to table.")
+        playerGuid = UnitGUID(nameFromGuild)
+
+        if rankIndex < 2 then
+            if DAL:AddToDKPTable(nameFromGuild, playerGuid, tempClass) then
+                print("ThirtyDKP: added "..nameFromGuild.." successfully to table.")
+            end
         end
     end
 end
--- DAL/DKPTable.lua
+++ DAL/DKPTable.lua
@@ -18,14 +18,92 @@ function DAL:InitializeDKPTable()
     
 end
 
-function DAL:AddToDKPTable(playerName, playerGuid, playerClass)
-    -- TODO: Sök igenom funktion och returnera false om player redan finns
-    tinsert(ThirtyDKP_Database_DKPTable, {
-        player=playerName,
-        guid=playerGuid,
-        class=playerClass,
-        dkp=0,
-    });
-    return true
+-- returns index if found
+-- else false
+function DAL:Table_Search(tar, val, field)
+	local value = string.upper(tostring(val));
+	local location = {}
+	for k,v in pairs(tar) do
+		if(type(v) == "table") then
+			local temp1 = k
+			for k,v in pairs(v) do
+				if(type(v) == "table") then
+					local temp2 = k;
+					for k,v in pairs(v) do
+						if(type(v) == "table") then
+							local temp3 = k
+							for k,v in pairs(v) do
+								if string.upper(tostring(v)) == value then
+									if field then
+										if k == field then
+											tinsert(location, {temp1, temp2, temp3, k} )
+										end
+									else
+										tinsert(location, {temp1, temp2, temp3, k} )
+									end
+								end;
+							end
+						end
+						if string.upper(tostring(v)) == value then
+							if field then
+								if k == field then
+									tinsert(location, {temp1, temp2, k} )
+								end
+							else
+								tinsert(location, {temp1, temp2, k} )
+							end
+						end;
+					end
+				end
+				if string.upper(tostring(v)) == value then
+					if field then
+						if k == field then
+							tinsert(location, {temp1, k} )
+						end
+					else
+						tinsert(location, {temp1, k} )
+					end
+				end;
+			end
+		end
+		if string.upper(tostring(v)) == value then
+			if field then
+				if k == field then
+					tinsert(location, k)
+				end
+			else
+				tinsert(location, k)
+			end
+		end;
+	end
+	if (#location > 0) then
+		return location;
+	else
+		return false;
+	end
 end
+
+function DAL:AddToDKPTable(playerName, playerGuid, playerClass)
+
+    --Will either contain index of player or false if not found
+    local playerExists = DAL:Table_Search(ThirtyDKP_Database_DKPTable, playerName)
+  
+    if playerExists == false then
+        tinsert(ThirtyDKP_Database_DKPTable, {
+            player=playerName,
+            guid=playerGuid,
+            class=playerClass,
+            dkp=0,
+        });
+
+        return true;
+    else
+        return false;
+    end
+    
+end
+
+function DAL:RemoveFromDKPTable(playerGuid)
+end
+
 

b323f2b185eb7032aaf2e129f3382f6dbc798352
Jonas Westlund <jonaswestlund101@gmail.com>
2020-06-13 22:58:34 +0200

    Add DKP per boss and item cost input options

--- View/OptionsFrame.lua
+++ View/OptionsFrame.lua
@@ -97,6 +97,39 @@ local function AttachAddGuildToTableScript(frame)
     end);
 end
 
+local function CreateInputFrame(text, parent)
+    local wrapper = CreateFrame("Frame", nil, parent, nil);
+    wrapper:SetSize(150, 30);
+
+    wrapper.label = wrapper:CreateFontString(nil, Const.OVERLAY_LAYER);
+    wrapper.label:SetFontObject("GameFontNormal");
+    wrapper.label:ClearAllPoints();
+    wrapper.label:SetText(text);
+    wrapper.label:SetPoint(Const.TOP_LEFT_POINT, wrapper, Const.TOP_LEFT_POINT, 0, -5)
+
+    wrapper.input = CreateFrame("EditBox", nil, wrapper, nil);
+    wrapper.input:SetFontObject("GameFontNormal");
+    wrapper.input:SetSize(30, 20);
+    wrapper.input:SetAutoFocus(false);
+    wrapper.input:SetNumeric(true);
+    wrapper.input:SetNumber(0);
+    wrapper.input:SetJustifyH("CENTER");
+    wrapper.input:SetPoint(Const.TOP_RIGHT_POINT, wrapper, Const.TOP_RIGHT_POINT, 0, 0);
+    wrapper.input:SetScript("OnEnterPressed", wrapper.input.ClearFocus);
+
+    local tex = wrapper.input:CreateTexture(nil, "BACKGROUND");
+    tex:SetAllPoints();
+    tex:SetColorTexture(0.2, 0.2, 0.2);
+
+    return wrapper;
+end
+
+local function CreateAndAttachInputFrame(text, parent, attachTarget)
+    local frame = CreateInputFrame(text, parent);
+    frame:SetPoint(Const.TOP_LEFT_POINT, attachTarget, Const.BOTTOMLEFT_POINT, 0, 0);
+    return frame;
+end
+
 function View:CreateOptionsFrame(parentFrame)
 	OptionsFrame = CreateFrame('Frame', 'ThirtyDKP_OptionsFrame', UIParent, "UIPanelDialogTemplate"); -- Todo: make mainframe owner??
 	OptionsFrame:SetShown(false);
@@ -112,6 +145,44 @@ function View:CreateOptionsFrame(parentFrame)
     title:ClearAllPoints();
     title:SetPoint(Const.TOP_LEFT_POINT, OptionsFrame, Const.TOP_LEFT_POINT, 15, -10);
     title:SetText(OPTIONS_FRAME_TITLE);
+
+
+    -- Misc
+    local dkpGainPerKill = CreateInputFrame("DKP Per Kill:", OptionsFrame);
+    dkpGainPerKill:SetPoint(Const.TOP_LEFT_POINT, OptionsFrame, Const.TOP_LEFT_POINT, 30, -50);
+
+
+    -- Item cost setting, two sections
+    local itemCostSectionLeft = CreateFrame("Frame", nil, OptionsFrame, nil);
+    itemCostSectionLeft:SetSize(180, 150);
+    itemCostSectionLeft:SetPoint(Const.TOP_LEFT_POINT, dkpGainPerKill, Const.BOTTOMLEFT_POINT, 0, -25);
+
+    local itemCostSectionRight = CreateFrame("Frame", nil, OptionsFrame, nil);
+    itemCostSectionRight:SetSize(180, 150);
+    itemCostSectionRight:SetPoint(Const.TOP_LEFT_POINT, itemCostSectionLeft, Const.TOP_RIGHT_POINT, 0, 0);
+
+    -- Left section
+    local headCostInput = CreateInputFrame("Head Cost:", itemCostSectionLeft, itemCostSectionLeft);
+    headCostInput:SetPoint(Const.TOP_LEFT_POINT, itemCostSectionLeft, Const.TOP_LEFT_POINT, 0, 0);
+
+    local neckCostInput = CreateAndAttachInputFrame("Neck Cost:", itemCostSectionLeft, headCostInput);
+    local shouldersCostInput = CreateAndAttachInputFrame("Shoulders Cost:", itemCostSectionLeft, neckCostInput);
+    local chestCostInput = CreateAndAttachInputFrame("Chest Cost:", itemCostSectionLeft, shouldersCostInput);
+    local bracersCostInput = CreateAndAttachInputFrame("Bracers Cost:", itemCostSectionLeft, chestCostInput);
+    local glovesCostInput = CreateAndAttachInputFrame("Gloves Cost:", itemCostSectionLeft, bracersCostInput);
+    local oneHandedWeaponCostInput = CreateAndAttachInputFrame("1h Weapon Cost:", itemCostSectionLeft, glovesCostInput);
+    local twoHandedWeaponCostInput = CreateAndAttachInputFrame("2h Weapon Cost:", itemCostSectionLeft, oneHandedWeaponCostInput);
+    local rangedWeaponCostInput = CreateAndAttachInputFrame("Ranged Cost:", itemCostSectionLeft, twoHandedWeaponCostInput);
+
+    -- Right section
+    local beltCostInput = CreateInputFrame("Belt Cost:", itemCostSectionRight, itemCostSectionRight);
+    beltCostInput:SetPoint(Const.TOP_LEFT_POINT, itemCostSectionRight, Const.TOP_LEFT_POINT, 0, 0);
+
+    local legsCostInput = CreateAndAttachInputFrame("Legs Cost:", itemCostSectionRight, beltCostInput);
+    local bootsCostInput = CreateAndAttachInputFrame("Boots Cost:", itemCostSectionRight, legsCostInput);
+    local ringCostInput = CreateAndAttachInputFrame("Ring Cost:", itemCostSectionRight, bootsCostInput);
+    local trinketCostInput = CreateAndAttachInputFrame("Trinket Cost:", itemCostSectionRight, ringCostInput);
+
 
     -- Buttons
     OptionsFrame.addRaidToTableBtn = CreateFrame("Button", nil, OptionsFrame, "GameMenuButtonTemplate");
